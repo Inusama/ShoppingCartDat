4dafffa46733f5c9a7f49665e844417e3912d61e
Inusama <12inusama>
2015-08-30 01:02:47 +0700

    1st Update

---
+++ ShoppingCart/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public string Index()
+        {
+            return "Hello from Shop.Index()";
+        }
+
+        // GET: /Shop/Browse
+        public string Browse(string category)
+        {
+            string message = HttpUtility.HtmlEncode("Shop.Browse, Category = " + category);
+            return message;
+        }
+
+        // GET: /Shop/Details
+        public string Details(int id)
+        {
+            string message = "Shop.Details, ID = " + id;
+            return message;
+        }
+    }
+}
